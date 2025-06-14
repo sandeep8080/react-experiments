6b6ea645a5f18767ca06cf36a0c3a659695bfc19
Sandeep Garg <sandeepgarg14319@gmail.com>
2025-06-14 23:08:24 +0530

    chore: initial commit for repo setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-8xl font-bold text-center">
+        React Experiment with Vite + TypeScript + Tailwind CSS
+      </h1>
+    </>
+  );
+}
+
+export default App;
